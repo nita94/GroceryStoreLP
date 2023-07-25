c97ead8556dedc28306b728b932c5c0131bd80f7
nickt <nicktan17@gmail.com>
2023-07-25 18:24:54 -0500

    completed phase 0 & 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; // Newly added import
+import configureStore from './store'; // Newly added import
 import './index.css';
 import App from './App';
 
+const store = configureStore(); // Newly added line
+
 function Root() {
   return (
+    <Provider store={store}> {/* Wrapping BrowserRouter with Provider */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
@@ -18,3 +24,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Exposing store during development
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
